c2aa1d8b0a04d4df84c83bb3d1edbbae715bebdb
zhaimengting <942170660@qq.com>
2018-06-30 11:34:28 +0800

    new
    呵呵
    
    Co-Authored-By: zhai <zhai@users.noreply.github.com>

--- TabbarDemo/MainViewController.m
+++ TabbarDemo/MainViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
+    NSLog(@"呃呃呃呃呃");
 }
 
 - (void)didReceiveMemoryWarning {
